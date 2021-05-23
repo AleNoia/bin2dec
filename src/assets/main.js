2ded7101898ac119b188848ea3294fbd195218bf
Igor Noia Silva <82424777+AleNoia@users.noreply.github.com>
2021-05-23 16:40:23 -0300

    Commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,40 @@
 function bin2dec() {
-    let dec = 0;
-    this.binSeletor = document.querySelector('.bin');
-    let bin = this.binSeletor.value
+    this.binSeletor = document.querySelector('.bin'); // Binary number
+    this.resSelector = document.querySelector('.result'); // Result div
+    let dec = 0; // Result
 
+    // ======================================== [START CONVERT]
     this.start = () => {
         this.binSeletor.focus();
-        for (var i = 0; i < bin.length; i++) {
-            dec = dec + parseInt(bin[i]) * Math.pow(2, bin.length - 1 - i);
-        }
+        this.listenConvertEvent();
+        this.listenEnter();
+    }
+    
+    // ======================================== [LISTEN THE CONVERT EVENT]
+    this.listenConvertEvent = () => {
+        document.addEventListener('click', e => {
+            const el = e.target;
+            if (el.classList.contains('convert')) this.convert();
+        })
     }
 
+    // ======================================== [LINTER ENTER TO CALL CONVERT FUNCTION]
+    this.listenEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.keyCode === 13) this.convert()
+        })
+    }
+
+    // ======================================== [CONVERT FUNCTION]
+    this.convert = () => {
+        let bin = this.binSeletor.value
+        for (var i = 0; i < bin.length; i++) {
+            // CONVERT
+            dec = dec + parseInt(bin[i]) * Math.pow(2, bin.length - 1 - i); // CONVERT
+        }
+        // PUTTING RESULTS IN THE DIV
+        this.resSelector.innerHTML = dec 
+    }
 }
 
 const binTwoDec = new bin2dec();
